8dd4a98db12ba13bfc362ae21146e6b9c880d1f4
vardevanyan1967-source <vardevanyan1967@mail.ru>
2026-05-25 16:35:51 +0400

    3

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE='imastun-v1';
+const ASSETS=[
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;0,700;1,400&display=swap'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  // Only cache GET requests
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(e.request).then(res=>{
+        if(!res||res.status!==200) return res;
+        const clone=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
+        return res;
+      }).catch(()=>cached);
+    })
+  );
+});
